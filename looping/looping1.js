8112d2a5f3bfad95fa87f2db950d68cedd25852c
AchmadMuafiTaufiqurrochman <withdraw14@gmail.com>
2023-12-17 21:41:14 +0700

    belajar looping

---
+++ looping/looping1.js
@@ -0,0 +1,22 @@
+// let i = 0;
+// while (i <= 3 ) { // menampilkan 0, lalu 1, lalu 2
+//   console.log( i );
+//   i++;
+// }
+
+// let i = 3;
+// while (i) { // ketika i menjadi 0, kondisi bernilai salah, dan perulangan berhenti
+//   console.log( i );
+//   i--;
+// }
+
+// let i = 0;
+// do {
+//   console.log("uwu " + i)
+//   i++;
+// } while (i < 5);
+
+for (let i = 0; i < 3; i++) { // menampilkan 0, lalu 1, lalu 2
+    console.log(i);
+  }
+
